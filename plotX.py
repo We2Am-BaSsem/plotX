68a7e649794dfda72d6f2fadf35e45d4a41f7f65
We2Am-BaSsem <58189568+We2Am-BaSsem@users.noreply.github.com>
2021-12-29 04:58:08 +0200

    plotX

--- plotX.py
+++ plotX.py
@@ -1,17 +1,18 @@
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QPixmap
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QApplication,
     QWidget,
     QLabel,
     QPushButton,
     QVBoxLayout,
+    QHBoxLayout,
     QTextEdit,
     QLineEdit,
 )
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
-from Components.TextField import TextField
 from Logic import Submit
 from functools import partial
 import matplotlib.pyplot as plt
@@ -27,13 +28,42 @@ class plotX(QWidget):
         self.setWindowTitle("plotX")
         self.layout = QVBoxLayout(self)
 
-        self.text_label = QLabel()
-        self.text_label.setText("")
-        self.text_label.setFont(QFont("Arial", 20))
+        self.F_X_label = QLabel()
+        self.F_X_label.setText("Function")
+        self.F_X_label.setFont(QFont("Arial", 10))
 
-        self.F_X = TextField()
-        self.minX = TextField()
-        self.maxX = TextField()
+        self.F_X = QLineEdit()
+        self.F_X.setFixedSize(1000, 20)
+
+        self.F_X_layout = QHBoxLayout()
+        self.F_X_layout.addStretch(1)
+        self.F_X_layout.addWidget(self.F_X_label)
+        self.F_X_layout.addWidget(self.F_X)
+        self.F_X_layout.alignment()
+
+        self.minX_label = QLabel()
+        self.minX_label.setText("min X")
+        self.minX_label.setFont(QFont("Arial", 10))
+
+        self.minX = QLineEdit()
+        self.minX.setFixedSize(1000, 20)
+
+        self.minX_layout = QHBoxLayout()
+        self.minX_layout.addStretch(1)
+        self.minX_layout.addWidget(self.minX_label)
+        self.minX_layout.addWidget(self.minX)
+
+        self.maxX_label = QLabel()
+        self.maxX_label.setText("max X")
+        self.maxX_label.setFont(QFont("Arial", 10))
+
+        self.maxX = QLineEdit()
+        self.maxX.setFixedSize(1000, 20)
+
+        self.maxX_layout = QHBoxLayout()
+        self.maxX_layout.addStretch(1)
+        self.maxX_layout.addWidget(self.maxX_label)
+        self.maxX_layout.addWidget(self.maxX)
 
         # self.canvas = canvas(self)
         self.figure = plt.figure()
@@ -44,7 +74,7 @@ class plotX(QWidget):
         self.PlotButton.setFont(QFont("Arial", 14))
         self.PlotButton.clicked.connect(
             lambda: Submit(
-                ErrorMessageLabel=self.text_label,
+                ErrorMessageLabel=self.error_message_label,
                 Function=self.F_X,
                 minX=self.minX,
                 maxX=self.maxX,
@@ -53,39 +83,26 @@ class plotX(QWidget):
             )
         )
 
+        self.error_message_label = QLabel()
+        self.error_message_label.setText("")
+        self.error_message_label.setFont(QFont("Arial", 20))
+        self.error_message_label.setStyleSheet("color: red")
+        self.error_message_label.setAlignment(Qt.AlignCenter)
+
+        self.logo = QLabel(self)
+        logo = QPixmap("logo.png")
+        self.logo.setPixmap(logo)
+        self.logo.resize(200, 200)
+        self.logo.setAlignment(Qt.AlignCenter)
+
         self.toolbar = NavigationToolbar(self.canvas, self)
 
-        self.layout.addWidget(self.F_X)
-        self.layout.addWidget(self.minX)
-        self.layout.addWidget(self.maxX)
+        self.layout.addLayout(self.F_X_layout)
+        self.layout.addLayout(self.minX_layout)
+        self.layout.addLayout(self.maxX_layout)
         self.layout.addWidget(self.PlotButton)
-        self.layout.addWidget(self.text_label)
+        self.layout.addWidget(self.logo)
+        self.layout.addWidget(self.error_message_label)
         self.layout.addWidget(self.canvas)
         self.layout.addWidget(self.toolbar)
         self.setLayout(self.layout)
-
-    # def Submit(self):
-    #     try:
-    #         ValidationResult = Validate(
-    #             self.F_X.text(), self.minX.text(), self.maxX.text()
-    #         )
-    #         if ValidationResult == "minX can't be greater than or equal to maxX":
-    #             self.text_label.setText(ValidationResult + ".\nThey will be swaped")
-    #             temp = self.minX.text()
-    #             self.minX.setText(self.maxX.text())
-    #             self.maxX.setText(temp)
-    #         elif ValidationResult != "OK":
-    #             self.text_label.setText(ValidationResult)
-    #             return
-    #         else:
-    #             self.text_label.setText("")
-
-    #         xAxis = np.linspace(float(self.minX.text()), float(self.maxX.text()), 100)
-    #         yAxis = f(self.F_X.text(), xAxis)
-
-    #         self.figure.clear()
-    #         ax = self.figure.add_subplot(111)
-    #         ax.plot(xAxis, yAxis)
-    #         self.canvas.draw()
-    #     except:
-    #         print("error")
--- test_app.py
+++ test_app.py
@@ -14,25 +14,25 @@ def app(qtbot):
 
 def test_submit_empty_data_raise_error(app, qtbot):
     qtbot.mouseClick(app.PlotButton, QtCore.Qt.LeftButton)
-    assert app.text_label.text() == "Empty Data"
+    assert app.error_message_label.text() == "Empty Data"
 
 
 def test_submit_empty_minX_and_maxX_raise_error(app, qtbot):
     qtbot.keyClicks(app.F_X, "x+2")
     qtbot.mouseClick(app.PlotButton, QtCore.Qt.LeftButton)
-    assert app.text_label.text() == "Empty Data"
+    assert app.error_message_label.text() == "Empty Data"
 
 
 def test_submit_empty_Function_and_maxX_raise_error(app, qtbot):
     qtbot.keyClicks(app.minX, "value")
     qtbot.mouseClick(app.PlotButton, QtCore.Qt.LeftButton)
-    assert app.text_label.text() == "Empty Data"
+    assert app.error_message_label.text() == "Empty Data"
 
 
 def test_submit_empty_minX_and_Function_raise_error(app, qtbot):
     qtbot.keyClicks(app.maxX, "value")
     qtbot.mouseClick(app.PlotButton, QtCore.Qt.LeftButton)
-    assert app.text_label.text() == "Empty Data"
+    assert app.error_message_label.text() == "Empty Data"
 
 
 def test_submit__string_minX__raise_error(app, qtbot):
@@ -40,7 +40,7 @@ def test_submit__string_minX__raise_error(app, qtbot):
     qtbot.keyClicks(app.minX, "value")
     qtbot.keyClicks(app.maxX, "-5")
     qtbot.mouseClick(app.PlotButton, QtCore.Qt.LeftButton)
-    assert app.text_label.text() == "Enter a numeric value for minX"
+    assert app.error_message_label.text() == "Enter a numeric value for minX"
 
 
 def test_submit__string_maxX__raise_error(app, qtbot):
@@ -48,7 +48,7 @@ def test_submit__string_maxX__raise_error(app, qtbot):
     qtbot.keyClicks(app.minX, "5.5")
     qtbot.keyClicks(app.maxX, "value")
     qtbot.mouseClick(app.PlotButton, QtCore.Qt.LeftButton)
-    assert app.text_label.text() == "Enter a numeric value for maxX"
+    assert app.error_message_label.text() == "Enter a numeric value for maxX"
 
 
 def test_submit__minX_greater_than_maxX__raise_error(app, qtbot):
@@ -57,7 +57,7 @@ def test_submit__minX_greater_than_maxX__raise_error(app, qtbot):
     qtbot.keyClicks(app.maxX, "1")
     qtbot.mouseClick(app.PlotButton, QtCore.Qt.LeftButton)
     assert (
-        app.text_label.text()
+        app.error_message_label.text()
         == "minX can't be greater than maxX.\nThey will be swaped"
     )
 
@@ -68,7 +68,7 @@ def test_submit__minX_equal_maxX__raise_error(app, qtbot):
     qtbot.keyClicks(app.maxX, "5.5")
     qtbot.mouseClick(app.PlotButton, QtCore.Qt.LeftButton)
     assert (
-        app.text_label.text()
+        app.error_message_label.text()
         == "minX can't be equal to to maxX"
     )
 
@@ -78,7 +78,7 @@ def test_submit__string_invalid_function__raise_error(app, qtbot):
     qtbot.keyClicks(app.minX, "5.5")
     qtbot.keyClicks(app.maxX, "66")
     qtbot.mouseClick(app.PlotButton, QtCore.Qt.LeftButton)
-    assert app.text_label.text() == "Invalid Process"
+    assert app.error_message_label.text() == "Invalid Process"
 
 
 def test_valid_submit__does_not_raise_error(app, qtbot):
@@ -86,4 +86,4 @@ def test_valid_submit__does_not_raise_error(app, qtbot):
     qtbot.keyClicks(app.minX, "5.5")
     qtbot.keyClicks(app.maxX, "66")
     qtbot.mouseClick(app.PlotButton, QtCore.Qt.LeftButton)
-    assert app.text_label.text() == ""
+    assert app.error_message_label.text() == ""
